cd180a3cd5af5937a72f67cfd465fd0ae8030893
alexgrek <alexgrek.hq@gmail.com>
2025-05-03 02:42:26 +0300

    with VR but still runs

---
+++ BotSimZero/VR/VrUtils.cs
@@ -0,0 +1,27 @@
+﻿using Stride.Core.Mathematics;
+using Stride.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotSimZero.VR
+{
+    public static class VrUtils
+    {
+        public static Vector3 LogicDirectionToWorldDirection(Vector2 logicDirection, CameraComponent camera, Vector3 upVector)
+        {
+            camera.Update();
+            var inverseView = Matrix.Invert(camera.ViewMatrix);
+
+            var forward = Vector3.Cross(upVector, inverseView.Right);
+            forward.Normalize();
+
+            var right = Vector3.Cross(forward, upVector);
+            var worldDirection = forward * logicDirection.Y + right * logicDirection.X;
+            worldDirection.Normalize();
+            return worldDirection;
+        }
+    }
+}
